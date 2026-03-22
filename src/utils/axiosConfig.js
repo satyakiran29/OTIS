a0f54c68810d62413bb48a171237bfb823d3308d
Satyakiran <87798342+satyakiran29@users.noreply.github.com>
2026-03-23 00:42:25 +0530

    Update axiosConfig.js

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    //baseURL: 'https://otis-api.onrender.com/api', // Use deployed backend for production
-    baseURL: 'http://localhost:5000/api', // Use local backend for development
+    baseURL: 'https://otis-api.onrender.com/api', // Use deployed backend for production
+    //baseURL: 'http://localhost:5000/api', // Use local backend for development
 });
 
 export default instance;
